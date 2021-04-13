25934d3eb3bb886c413ffb5c1ae6d18d678142db
Asadullah-Dal17 <aiphile17@gmail.com>
2021-04-13 14:49:54 +0500

    adding landmarks detection

--- Module.py
+++ Module.py
@@ -17,33 +17,45 @@ ORANGE = (0,69,255)
 # objects 
 detectFace= dlib.get_frontal_face_detector()
 # landmarks detector  
-predictor = dlib.shape_predictor('Predictor/shape_predictor_68_face_landmarks.dat')
+predictor = dlib.shape_predictor("Predictor/shape_predictor_68_face_landmarks.dat")
 
 # function
-
 def faceDetector(image, Draw =True):
     pts1=(0,0)
     pts2=(0,0)
     IDs=0
     Gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     faces = detectFace(Gray)
-    face= []
-    for IDs, face in enumerate(faces):
-        print(face)
+    face= None
+    for face  in faces:
+        # print(face)
         pts1 = (face.left(), face.top())
         pts2 = (face.right(), face.bottom())
+        # landmarks = predictor(Gray, face)
+        # x=landmarks.part(36).x
+        # y=landmarks.part(36).y
+        # print(type(landmarks))
         if Draw ==True:
             cv.rectangle(image, pts1, pts2, (0,255,255), 1)
-            if IDs ==1:
-                cv.rectangle(image,pts1, pts2, (255,255,255), 3)
+            # if IDs ==1:
+                # cv.rectangle(image,pts1, pts2, (255,255,255), 3)
     # print(data)
     return image,face
-
-def BlinkingDetection(GrayImage,face ):
-    landmarks = predictor(GrayImage, face)
+def facePoint(grayImage, image,face):
+    landmarks =predictor(grayImage, face)
+    pointsList = []
+    for n in range(0,68):
+        point = (landmarks.part(n).x, landmarks.part(n).y)
+        pointsList.append(point)
+        cv.circle(image,point, 2, ORANGE, 2)
+    print(pointsList)
+    # print(point)
+    return image, pointsList
+# def BlinkingDetection(GrayImage,face ):
+    # landmarks = predictor(GrayImage, face)
     print('working')
-    # firstPoint = (landmarks.part[42].x, landmarks.part[42].y)
-    # print(firstPoint)
+    # x, y = landmarks.part[10].x, landmarks.part[10].y
+    # print(x, y)
 
 
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import Module as m 
 import time
+import dlib
 
 # variables
 fonts = cv.FONT_HERSHEY_PLAIN
@@ -9,15 +10,30 @@ frameCounter = 0
 capID=1 
 # objects  
 camera = cv.VideoCapture(capID)
+# predictor = dlib.shape_predictor('Predictor/shape_predictor_68_face_landmarks.dat')
 staringTimer = time.time()
 while True:
     frameCounter += 1
     ret, frame= camera.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow("image", frame)
+    # cv.imshow("image", frame)
     image,data =m.faceDetector(frame, Draw=True)
-    if len(data)>0:
-        m.BlinkingDetection(gray, data)
+    if data is not None:
+        # m.BlinkingDetection(gray, data)
+        print('detected')
+        image, PointLis =m.facePoint(gray, frame, data)
+        # print(type(point))
+        # cv.circle(frame, point,5, m.CHOCOLATE, 2 )
+        # print(data)
+        # n =1
+        # landmarks = (gray, data)
+        # x=landmarks.part(n).x
+        # print(type(landmarks))
+        # print(landmarks)
+        # x, y = landmarks.part(1).x,landmarks.part(1).y
+        # print(x, y)
+    # if len(data)>0:
+        # m.BlinkingDetection(gray, data)
     # if len(data) >0:
         # print(data)
     seconds = time.time() - staringTimer
