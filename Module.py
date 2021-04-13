7af988ddaa6c4aae51031bae30d25b399377f2b2
Asadullah-Dal17 <aiphile17@gmail.com>
2021-04-13 10:59:46 +0500

    adding face detection funtionality

--- Module.py
+++ Module.py
@@ -2,4 +2,23 @@ import cv2 as cv
 import numpy as np 
 import dlib
 
-faceDetector=
+# variables
+
+# colors 
+
+# objects 
+detectFace= dlib.get_frontal_face_detector()
+
+# function
+
+def faceDetector(image, Draw =True):
+    Gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    faces = detectFace(Gray)
+    for IDs, face in enumerate(faces):
+        X, Y = face.left(), face.top()
+        X1, Y1 = face.right(), face.bottom()
+        if Draw ==True:
+            cv.rectangle(image, (X, Y), (X1, Y1), (0,255,255), 1)
+    return X, Y, X1, Y1
+  
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 as cv 
 import numpy as np
 import dlib
-
+import Module as m 
 # variables
 capID=1 
 # objects  
@@ -10,6 +10,8 @@ camera = cv.VideoCapture(capID)
 while True:
     ret, frame= camera.read()
     cv.imshow("image", frame)
+    image =m.faceDetector(frame, Draw=True)
+    cv.imshow("image", image)
     key =cv.waitKey(1)
     if key ==ord('q'):
         break
