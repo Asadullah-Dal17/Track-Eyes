1a5d1339fdd40f76112be67e4e33c734a1fc410f
Asadullah-Dal17 <aiphile17@gmail.com>
2021-04-13 18:42:47 +0500

    completed Blinking function

--- Module.py
+++ Module.py
@@ -1,6 +1,7 @@
 import cv2 as cv 
 import numpy as np 
 import dlib
+import math
 # variables
 
 # colors 
@@ -13,6 +14,9 @@ PURPLE = (128,0,128)
 CHOCOLATE = (30,105,210)
 PINK = (147,20,255)
 ORANGE = (0,69,255)
+GREEN =(0,255,0)
+BLUE = (255, 0, 0)
+RED = (0, 0, 255)
 # objects 
 detectFace= dlib.get_frontal_face_detector()
 # landmarks detector  
@@ -22,6 +26,18 @@ predictor = dlib.shape_predictor("Predictor/shape_predictor_68_face_landmarks.da
 def midpoint(pts1, pts2):
     x, y = pts1
     x1, y1 = pts2
+    xOut=int((x + x1)/2)
+    yOut = int((y1 + y)/2)
+    # print(xOut, x, x1)
+    return (xOut, yOut)
+
+def eucaldainDistance(pts1, pts2):
+    x, y =pts1
+    x1, y1 =pts2
+    eucaldainDist = math.sqrt((x1 - x) ** 2 + (y1 - y) ** 2)
+    
+    return eucaldainDist
+
 def faceDetector(image, Draw =True):
     pts1=(0,0)
     pts2=(0,0)
@@ -54,7 +70,16 @@ def facePoint(grayImage, image,face, Draw=False):
     return image,pointsList
 
 def blinkDetector(eyePoints):
+    #TODO find blinking points 
+    top = eyePoints[1:3]
+    bottom = eyePoints[4:6]
+    topMid = midpoint(top[0], top[1])
+    bottomMid = midpoint(bottom[0], bottom[1])
+    # print(topMid)
+    Vertical =eucaldainDistance(topMid, bottomMid)
+    Horizontal =eucaldainDistance(eyePoints[0], eyePoints[3])
+    blinkRatio = (Horizontal/Vertical)
 
-
-
-    
+    # print(Vertical, '   ', Horizontal)
+    # return top, bottom,topMid, bottomMid 
+    return blinkRatio
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import cv2 as cv 
 import numpy as np
-import Module as m 
+import Module as m
 import time
 import dlib
 
 # variables
 fonts = cv.FONT_HERSHEY_PLAIN
 frameCounter = 0
-capID=1 
+capID=0 
 # objects  
 camera = cv.VideoCapture(capID)
-# predictor = dlib.shape_predictor('Predictor/shape_predictor_68_face_landmarks.dat')
+# staring time 
 staringTimer = time.time()
 while True:
     frameCounter += 1
@@ -21,13 +21,29 @@ while True:
     if data is not None:
         # m.BlinkingDetection(gray, data)
         # print('detected')
+        
         image, PointLis =m.facePoint(gray, frame, data)
-        cv.circle(frame, PointLis[0], 3,m.LIGHT_BLUE, 2)
+        eyesPoints= PointLis[36:42]
+        BlinkRatio=m.blinkDetector(eyesPoints)
+        # print(BlinkRatio)
+        if BlinkRatio<=3:
+            print("no Blink")
+        elif BlinkRatio>3:
+            print("Eyes Blinked")
+        # cv.circle(frame, bPoints[0], 3, m.GREEN, 2)
+        # cv.circle(frame, topMid, 3, m.YELLOW, 2)
+        # cv.circle(frame, bottomMid, 3, m.CYAN, 2)
+        # cv.circle(frame, bPoints[1], 3, m.RED, 2)
+        # cv.circle(frame, eyesPoints[0], 4, m.BLUE, 2)
+        # cv.circle(frame, eyesPoints[3], 4, m.BLUE, 2)
+        # for center in eyesPoints:
+            # cv.circle(frame, center,3, m.ORANGE, 1)
+        # cv.circle(frame, PointLis[0], 2,m.LIGHT_BLUE, 1)
+
     seconds = time.time() - staringTimer
     # print(seconds)
     fps = frameCounter/seconds
     # print(fps)
-    
     cv.putText(frame, f'FPS: {round(fps, 2)}',(20,20),fonts, 1.5, m.MAGENTA,2 )
     cv.imshow("image", image)
     key =cv.waitKey(1)
