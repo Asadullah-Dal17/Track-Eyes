00fa6019e444ec151980e7f2037e0650541e0ae7
Asadullah-Dal17 <aiphile17@gmail.com>
2021-04-15 06:08:37 +0500

    updating module and main.py

--- Module.py
+++ Module.py
@@ -26,97 +26,100 @@ predictor = dlib.shape_predictor("Predictor/shape_predictor_68_face_landmarks.da
 
 # function
 def midpoint(pts1, pts2):
-    x, y = pts1
-    x1, y1 = pts2
-    xOut=int((x + x1)/2)
-    yOut = int((y1 + y)/2)
-    # print(xOut, x, x1)
-    return (xOut, yOut)
+	x, y = pts1
+	x1, y1 = pts2
+	xOut=int((x + x1)/2)
+	yOut = int((y1 + y)/2)
+	# print(xOut, x, x1)
+	return (xOut, yOut)
 
 def eucaldainDistance(pts1, pts2):
-    x, y =pts1
-    x1, y1 =pts2
-    eucaldainDist = math.sqrt((x1 - x) ** 2 + (y1 - y) ** 2)
-    
-    return eucaldainDist
+	x, y =pts1
+	x1, y1 =pts2
+	eucaldainDist = math.sqrt((x1 - x) ** 2 + (y1 - y) ** 2)
+	
+	return eucaldainDist
 
 def faceDetector(image, Draw =True):
-    pts1=(0,0)
-    pts2=(0,0)
-    IDs=0
-    Gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    faces = detectFace(Gray)
-    face= None
-    for face  in faces:
-        # print(face)
-        pts1 = (face.left(), face.top())
-        pts2 = (face.right(), face.bottom())
-        # landmarks = predictor(Gray, face)
-        # x=landmarks.part(36).x
-        # y=landmarks.part(36).y
-        # print(type(landmarks))
-        if Draw ==True:
-            cv.rectangle(image, pts1, pts2, (0,255,255), 1)
-    # print(data)
-    return image,face
+	pts1=(0,0)
+	pts2=(0,0)
+	IDs=0
+	Gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+	faces = detectFace(Gray)
+	face= None
+	for face  in faces:
+		# print(face)
+		pts1 = (face.left(), face.top())
+		pts2 = (face.right(), face.bottom())
+		# landmarks = predictor(Gray, face)
+		# x=landmarks.part(36).x
+		# y=landmarks.part(36).y
+		# print(type(landmarks))
+		if Draw ==True:
+			cv.rectangle(image, pts1, pts2, (0,255,255), 1)
+	# print(data)
+	return image,face
 def facePoint(grayImage, image,face, Draw=False):
-    landmarks =predictor(grayImage, face,)
-    pointsList = []
-    for n in range(0,68):
-        point = (landmarks.part(n).x, landmarks.part(n).y)
-        pointsList.append(point)
-        if Draw ==True:
-            cv.circle(image,point, 2, ORANGE, 2)
-    # print(pointsList)
-    # print(point)
-    return image,pointsList
+	landmarks =predictor(grayImage, face,)
+	pointsList = []
+	for n in range(0,68):
+		point = (landmarks.part(n).x, landmarks.part(n).y)
+		pointsList.append(point)
+		if Draw ==True:
+			cv.circle(image,point, 2, ORANGE, 2)
+	# print(pointsList)
+	# print(point)
+	return image,pointsList
 
 def blinkDetector(eyePoints):
-    #TODO find blinking points 
-    top = eyePoints[1:3]
-    bottom = eyePoints[4:6]
-    topMid = midpoint(top[0], top[1])
-    bottomMid = midpoint(bottom[0], bottom[1])
-    # print(topMid)
-    Vertical =eucaldainDistance(topMid, bottomMid)
-    Horizontal =eucaldainDistance(eyePoints[0], eyePoints[3])
-    blinkRatio = (Horizontal/Vertical)
+	#TODO find blinking points 
+	top = eyePoints[1:3]
+	bottom = eyePoints[4:6]
+	topMid = midpoint(top[0], top[1])
+	bottomMid = midpoint(bottom[0], bottom[1])
+	# print(topMid)
+	Vertical =eucaldainDistance(topMid, bottomMid)
+	Horizontal =eucaldainDistance(eyePoints[0], eyePoints[3])
+	blinkRatio = (Horizontal/Vertical)
 
-    # print(Vertical, '   ', Horizontal)
-    # return top, bottom,topMid, bottomMid 
-    return blinkRatio
+	# print(Vertical, '   ', Horizontal)
+	# return top, bottom,topMid, bottomMid 
+	return blinkRatio
 
 def EyesTracking(image, EyesPoints, Draw =True):
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    dim = gray.shape 
-    mask = np.zeros(dim, dtype=np.uint8)
-    # print('noting mask')
-    # Drawing Right on the mask 
-    RightPolly = np.array(EyesPoints, dtype=np.int32)
+	gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+	dim = gray.shape 
+	mask = np.zeros(dim, dtype=np.uint8)
+	# print('noting mask')
+	# Drawing Right on the mask 
+	RightPolly = np.array(EyesPoints, dtype=np.int32)
 
-    # Fill the Region of Eye on the mask
-    cv.fillPoly(mask, [RightPolly], 255)
+	# Fill the Region of Eye on the mask
+	cv.fillPoly(mask, [RightPolly], 255)
 
-    # Extrating the Eyes from the image using and operation 
-    eye = cv.bitwise_and(gray, gray, mask=mask)
-    # TODO Extract the eyes form frame,
-    # find the mix and min x and y values of Eyes Positions 
-    # print(EyesPoints[0][0])
-    maxX =( max(EyesPoints, key=lambda item: item[0]))[0]
-    minX =( min(EyesPoints, key=lambda item: item[0]))[0]
-    maxY = (max(EyesPoints, key=lambda item: item[1]))[1]
-    minY = (min(EyesPoints, key=lambda item: item[1]))[1]
-    # print(EyesPoints, '    ', minX, minY, maxX, maxY)
-    
-    # Croping Eye from the frame
-    cropedEye = eye[minY:maxY, minX:maxX]
+	# Extrating the Eyes from the image using and operation 
+	eye = cv.bitwise_and(gray, gray, mask=mask)
+	# TODO Extract the eyes form frame,
+	# find the mix and min x and y values of Eyes Positions 
+	# print(EyesPoints[0][0])
+	maxX =( max(EyesPoints, key=lambda item: item[0]))[0]
+	minX =( min(EyesPoints, key=lambda item: item[0]))[0]
+	maxY = (max(EyesPoints, key=lambda item: item[1]))[1]
+	minY = (min(EyesPoints, key=lambda item: item[1]))[1]
+	# print(EyesPoints, '    ', minX, minY, maxX, maxY)
+	
+	# Croping Eye from the frame
+	cropedEye = eye[minY:maxY, minX:maxX]
 
-    # TODO apply Thereshold
+	# TODO apply Thereshold
 
-    # Apply Threshold to the Eyes inorder to Extract White and Black pixel
-    _, threshEye = cv.threshold(cropedEye,100,255, cv.ADAPTIVE_THRESH_GAUSSIAN_C)
-    # TODO count the WHITE Pixel in the image Eyes part and decide where eyes are looking
-    # TODO Improve the Visual of Information about Eyes 
-
-    return mask, threshEye
+	# Apply Threshold to the Eyes inorder to Extract White and Black pixel
+	ret,threshEye = cv.threshold(cropedEye,127,255,cv.THRESH_BINARY)
+	PixelWhite = np.sum(threshEye==255)
+	PixelBlack = np.sum(threshEye==0)
+	print(PixelBlack, '  ', PixelWhite)
+		# TODO count the WHITE Pixel in the image Eyes part and decide where eyes are looking
+	# TODO Improve the Visual of Information about Eyes 
+	
+	return mask, threshEye
 
--- main.py
+++ main.py
@@ -13,53 +13,53 @@ camera = cv.VideoCapture(capID)
 # staring time 
 staringTimer = time.time()
 while True:
-    frameCounter += 1
-    ret, frame= camera.read()
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # cv.imshow("image", frame)
-    image,data =m.faceDetector(frame, Draw=True)
-    if data is not None:
-        # m.BlinkingDetection(gray, data)
-        # print('detected')
-        
-        image, PointsList =m.facePoint(gray, frame, data)
-        LeftEyeList= PointsList[36:42]
-        RightEyeList = PointsList[42:48]
-        # ArryN = np.array(RightEyeList, dtype=np.int32)
-        # print(ArryN)
-        # mask = np.zeros(frame.shape, dtype=np.uint8)
-        # cv.fillPoly(mask, [ArryN], (0,255,255))
-        RightEye =m.blinkDetector(PointsList[42:48])
-        mask , RightCropd= m.EyesTracking(frame, LeftEyeList)
-        if mask is not None:
-            cv.imshow('mask', mask)
-            cv.imshow('Right', RightCropd)
-        LeftEye=m.blinkDetector(LeftEyeList)
-        BlinkRatio = (RightEye+LeftEye)/2
-        # print(BlinkRatio)
-        cv.circle(frame, (40, 40), int(BlinkRatio*3), m.LIGHT_BLUE, 3)
-        # cv.line(frame,(40, 70), (40,int(70-(BlinkRatio*6))),m.ORANGE,10)
-        if BlinkRatio>=4:
-            cv.putText(frame, f'Blink',(50, 70), cv.FONT_HERSHEY_COMPLEX, 0.9, m.PINK, 2 )
-        # cv.circle(frame, RightEyeList[0], 3, m.GREEN, 2)
-        # cv.circle(frame, RightEyeList[1], 3, m.YELLOW, 2)
-        # 
-        # cv.circle(frame, RightEyeList[3], 3, m.RED, 2)
-        # cv.circle(frame, RightEyeList[4], 4, m.BLUE, 2)
-        # cv.circle(frame, RightEyeList[5], 4, m.LIGHT_BLUE, 2)
-        # for center in LeftEyeList:
-            # cv.circle(frame, center,3, m.ORANGE, 1)
-        # cv.circle(frame, PointLis[0], 2,m.LIGHT_BLUE, 1)
+	frameCounter += 1
+	ret, frame= camera.read()
+	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	# cv.imshow("image", frame)
+	image,data =m.faceDetector(frame, Draw=True)
+	if data is not None:
+		# m.BlinkingDetection(gray, data)
+		# print('detected')
+		
+		image, PointsList =m.facePoint(gray, frame, data)
+		LeftEyeList= PointsList[36:42]
+		RightEyeList = PointsList[42:48]
+		# ArryN = np.array(RightEyeList, dtype=np.int32)
+		# print(ArryN)
+		# mask = np.zeros(frame.shape, dtype=np.uint8)
+		# cv.fillPoly(mask, [ArryN], (0,255,255))
+		RightEye =m.blinkDetector(PointsList[42:48])
+		mask , RightCropd= m.EyesTracking(frame, LeftEyeList)
+		if mask is not None:
+			cv.imshow('mask', mask)
+			cv.imshow('Right', RightCropd)
+		LeftEye=m.blinkDetector(LeftEyeList)
+		BlinkRatio = (RightEye+LeftEye)/2
+		# print(BlinkRatio)
+		cv.circle(frame, (40, 40), int(BlinkRatio*3), m.LIGHT_BLUE, 3)
+		# cv.line(frame,(40, 70), (40,int(70-(BlinkRatio*6))),m.ORANGE,10)
+		if BlinkRatio>=4:
+			cv.putText(frame, f'Blink',(50, 70), cv.FONT_HERSHEY_COMPLEX, 0.9, m.PINK, 2 )
+		# cv.circle(frame, RightEyeList[0], 3, m.GREEN, 2)
+		# cv.circle(frame, RightEyeList[1], 3, m.YELLOW, 2)
+		# 
+		# cv.circle(frame, RightEyeList[3], 3, m.RED, 2)
+		# cv.circle(frame, RightEyeList[4], 4, m.BLUE, 2)
+		# cv.circle(frame, RightEyeList[5], 4, m.LIGHT_BLUE, 2)
+		# for center in LeftEyeList:
+			# cv.circle(frame, center,3, m.ORANGE, 1)
+		# cv.circle(frame, PointLis[0], 2,m.LIGHT_BLUE, 1)
 
-    seconds = time.time() - staringTimer
-    # print(seconds)
-    fps = frameCounter/seconds
-    # print(fps)
-    cv.putText(frame, f'FPS: {round(fps, 2)}',(20,20),fonts, 1.5, m.MAGENTA,2 )
-    cv.imshow("image", image)
-    
-    key =cv.waitKey(1)
-    if key ==ord('q'):
-        break
+	seconds = time.time() - staringTimer
+	# print(seconds)
+	fps = frameCounter/seconds
+	# print(fps)
+	cv.putText(frame, f'FPS: {round(fps, 2)}',(20,20),fonts, 1.5, m.MAGENTA,2 )
+	cv.imshow("image", image)
+	
+	key =cv.waitKey(1)
+	if key ==ord('q'):
+		break
 cv.destroyAllWindows()
 camera.release()
